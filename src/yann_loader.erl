80326375e32d07313fe9eb6c3e06c0a7c6f7f2b2
Callum David O'Brien <callum.obrien@bluefruit.co.uk>
2018-03-11 10:16:01 +0000

    first stab at yann loader

--- src/yann_loader.erl
+++ src/yann_loader.erl
@@ -1,31 +1,56 @@
 -module(yann_loader).
 
--export([init/0, loader/1, activation/3]).
+-import(lists, []).
+-import(file, []).
+
+-export([init/0, loader/2]).
 
 init() ->
-   spawn_link(?MODULE, neuron, [0, [], []]).
+   loader("", []).
 
-
-
-loader(File_Location) -> %{ok, Binary} | {error, Reason}.
-   File = file:read_file(File_Liocation), 
-   [File] = [File] -- [0,0,28,28],
-    
-
-neuron(Bias, Weights, Inputs) ->
+loader(Path, DataSet) ->
    receive
-      {Sender, update, NewInputs} ->
-         Sender ! ok,
-         neuron(Bias, Weights, NewInputs);
+      {Sender, load, Path} ->
+         try load_data(Path) of
+            LoadedData ->
+               Sender ! ok,
+               loader(Path, DataSet ++ LoadedData)
+         catch _ ->
+            Sender ! {error, "couldn't read"},
+            loader(Path, DataSet)
+         end;
 
-      {Sender, train, BiasChange, WeightChanges} ->
-         Sender ! ok,
-         NewBias = Bias + BiasChange,
-         NewWeights = lists:zipWith(fun(X, Y) -> X + Y end, WeightChanges, Weights),
-         neuron(NewBias, NewWeights, Inputs);
+      {Sender, get, Count} when Count =< length(DataSet) ->
+         Sender ! {ok, lists:sublist(DataSet, Count)},
+         NewDataSet = lists:nthtail(Count, DataSet),
+         loader(Path, NewDataSet);
+      {Sender, get, Count} when Count > length(DataSet) ->
+         Sender ! fail,
+         loader(Path, DataSet)
+   end.
 
-      {Sender, get} ->
-         Activation = activation(Bias, Weights, Inputs),
-         Sender ! {ok, Activation},
-         neuron(Bias, Weights, Inputs)
-end.
+load_data(Path) ->
+   PathToLabelsFile = Path ++ "labels.bin",
+   {ok, <<_:64, LabelsBinary>>} = file:read_file(PathToLabelsFile),
+   Labels = parse_labels_binary(LabelsBinary),
+   PathToImagesFile = Path ++ "images.bin",
+   {ok, <<_:128, ImagesBinary>>} = file:read_file(PathToImagesFile),
+   Images = parse_images_binary(ImagesBinary),
+   lists:zip(Labels, Images).
+
+parse_labels_binary(<<>>) -> [];
+parse_labels_binary(<<LabelByte:8, LabelsBinary/binary>>) ->
+   <<Label/integer-unsigned-big>> = LabelByte,
+   [Label|parse_labels_binary(LabelsBinary)].
+
+parse_images_binary(<<>>) -> [];
+parse_images_binary(<<ImageBinary:6272, ImagesBinary/binary>>) ->
+   Image = parse_image_binary(ImageBinary),
+   [Image|parse_images_binary(ImagesBinary)].
+
+parse_image_binary(<<>>) -> [];
+parse_image_binary(<<PixelByte:8, ImageBinary/binary>>) ->
+   <<Pixel/integer-unsigned-big>> = PixelByte,
+   Brightness = Pixel / 255.0,
+   [Brightness|parse_image_binary(ImageBinary)].
+
