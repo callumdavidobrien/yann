60edf24b989bf22f72aff578ddb00a94232c2e96
Callum David O'Brien <callum.obrien@bluefruit.co.uk>
2018-03-11 05:15:49 +0000

    make neuron decent

--- src/yann_neuron.erl
+++ src/yann_neuron.erl
@@ -1,9 +1,11 @@
 -module(yann_neuron).
 -import(lists, [zipwith/3, sum/1]).
--export([init/0, neuron/3, activation/3]).
+-export([init/1, neuron/3, activation/3]).
 
-init() ->
-   spawn_link(?MODULE, neuron, [0, [], []]).
+init(InputSize) ->
+   Weights = lists:duplicate(InputSize, 0),
+   Inputs = lists:duplicate(InputSize, 0),
+   spawn_link(?MODULE, neuron, [0, Weights, Inputs]).
 
 neuron(Bias, Weights, Inputs) ->
    receive
@@ -14,12 +16,21 @@ neuron(Bias, Weights, Inputs) ->
       {Sender, train, BiasChange, WeightChanges} ->
          Sender ! ok,
          NewBias = Bias + BiasChange,
-         NewWeights = lists:zipWith(fun(X, Y) -> X + Y end, WeightChanges, Weights),
+         NewWeights = lists:zipwith(fun(X, Y) -> X + Y end, WeightChanges, Weights),
          neuron(NewBias, NewWeights, Inputs);
 
       {Sender, get} ->
          Activation = activation(Bias, Weights, Inputs),
          Sender ! {ok, Activation},
+         neuron(Bias, Weights, Inputs);
+
+
+      {Sender, ping} ->
+         Sender ! ok,
+         neuron(Bias, Weights, Inputs);
+
+      _ ->
+         io:format("neuron : invalid message"),
          neuron(Bias, Weights, Inputs)
    end.
 
