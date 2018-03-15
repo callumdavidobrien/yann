92f0998fd85f4e454e57f0dd04a86dd1194c6454
Callum David O'Brien <cdo@teknik.io>
2018-03-15 21:22:57 +0000

    write wrapper functions around messages in yann_neuron

--- src/yann_neuron.erl
+++ src/yann_neuron.erl
@@ -7,7 +7,7 @@
 -import(math, [exp/1]).
 -import(rand, [uniform/0]).
 
--export([init/1, get_bias/1, get_weight/2, neuron/3]).
+-export([init/1, update/2, change_bias/2, change_weight/3, get_activation/1, get_bias/1, get_weight/2, kill/1, neuron/3]).
 
 init(InputSize) ->
    Weights = uniform_randoms(InputSize, {-1, 1}),
@@ -22,6 +22,36 @@ uniform_randoms(Size, {Min, Max}) ->
    Array = new(Size),
    map(Random, Array).
 
+update(NeuronPid, Inputs) ->
+   NeuronPid ! {self(), update, Inputs},
+   receive
+      ok -> ok;
+      bad_input_size -> erlang:error(bad_neuron_input_size)
+   after 1000 -> erlang:error(timeout)
+   end.
+
+change_bias(NeuronPid, Change) ->
+   NeuronPid ! {self(), change_bias, Change},
+   receive
+      ok -> ok
+   after 1000 -> erlang:error(timeout)
+   end.
+
+change_weight(NeuronPid, Index, Change) ->
+   NeuronPid ! {self(), change_weight, Index, Change},
+   receive
+      ok -> ok;
+      bad_index -> erlang:error(bad_neuron_weight_index)
+   after 1000 -> erlang:error(timeout)
+   end.
+
+get_activation(NeuronPid) ->
+   NeuronPid ! {self(), get_activation},
+   receive
+      {ok, Activation} -> Activation
+   after 1000 -> erlang:error(timeout)
+   end.
+
 get_bias(NeuronPid) ->
    NeuronPid ! {self(), get_bias},
    receive
@@ -34,6 +64,13 @@ get_weight(NeuronPid, Index) ->
    receive
       {ok, Weight} -> Weight;
       bad_index -> erlang:error(bad_neuron_weight_index)
+   after 1000 -> erlang:error(timeout)
+   end.
+
+kill(NeuronPid) ->
+   NeuronPid ! {self(), die},
+   receive
+      ok -> ok
    after 1000 -> erlang:error(timeout)
    end.
 
