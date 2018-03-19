3daeb7c69fca885dd539b5c580b6b5eb97b6f7b4
Callum David O'Brien <cdo@teknik.io>
2018-03-19 19:35:51 +0000

    add first test for yann_neuron

--- tests/yann_neuron_tests.erl
+++ tests/yann_neuron_tests.erl
@@ -1,9 +1,32 @@
 -module(yann_neuron_tests).
 
+-include_lib("eqc/include/eqc.hrl").
+
 -compile({no_auto_import, [size/1]}).
 
--import(array, [foldl/3, from_list/1, size/1])
+-import(array, [foldl/3, from_list/1, map/2, new/1, size/1, to_list/1]).
+-import(rand, [uniform/0]).
+
 -import(yann_neuron, [init/1, update/2, change_bias/2, change_weight/3, get_activation/1, get_bias/1, get_weight/2, kill/1, neuron/3]).
 
--include_lib("eqc/include/eqc.hrl").
+-export([yann_neuron_tests/0, a_neuron_can_be_created_with_input_size/1]).
+
+yann_neuron_tests() ->
+   quickcheck(a_neuron_can_be_created_with_input_size_property()).
+
+% Tests %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+a_neuron_can_be_created_with_input_size(InputSize) ->
+   NeuronPid = init(InputSize),
+   NeuronPid ! {self(), invalid_message},
+   receive
+      bad_message -> true
+   after 2000 -> false
+   end.
+
+a_neuron_can_be_created_with_input_size_property() ->
+   ?FORALL(
+      InputSize,
+      nat(),
+      a_neuron_can_be_created_with_input_size(InputSize)).
 
