dbee631240745dd06059681649e6000ad2f6a434
Callum David O'Brien <cdo@teknik.io>
2018-03-21 17:28:05 +0000

    add tests for yann_neuron: kill/1, change_bias/2, change_weight/3, update/2 to yann_neuron_tests

--- tests/yann_neuron_tests.erl
+++ tests/yann_neuron_tests.erl
@@ -9,13 +9,17 @@
 
 -import(yann_neuron, [init/1, update/2, change_bias/2, change_weight/3, get_activation/1, get_bias/1, get_weight/2, kill/1, neuron/3]).
 
--export([run_all_tests/0, a_neuron_can_be_created_with_input_size/1, activation_of_neuron_can_be_got/1, bias_of_neuron_can_be_got/1, weight_of_neuron_for_nth_input_can_be_got/2]).
+-export([run_all_tests/0, a_neuron_can_be_created_with_input_size/1, activation_of_neuron_can_be_got/1, bias_of_neuron_can_be_got/1, weight_of_neuron_for_nth_input_can_be_got/2, bias_of_neuron_can_be_changed/2, weight_of_neuron_for_nth_input_can_be_changed/3, inputs_of_neuron_can_be_updated_to/2]).
 
 run_all_tests() ->
    quickcheck(property_any_neuron_can_be_created_with_input_size()),
    quickcheck(property_any_neurons_activation_can_be_got()),
    quickcheck(property_any_neurons_bias_can_be_got()),
-   quickcheck(property_any_neurons_weight_for_any_of_its_inputs_can_be_got()).
+   quickcheck(property_any_neurons_weight_for_any_of_its_inputs_can_be_got()),
+   quickcheck(property_any_neuron_can_be_killed()),
+   quickcheck(property_any_neurons_bias_can_be_changed()),
+   quickcheck(property_any_neurons_weight_for_any_of_its_inputs_can_be_changed()),
+   quickcheck(property_the_inputs_of_any_neuron_can_be_updated()).
 
 property_any_neuron_can_be_created_with_input_size() ->
    ?FORALL(
@@ -41,6 +45,30 @@ property_any_neurons_weight_for_any_of_its_inputs_can_be_got() ->
       neuron_index_pair(),
       weight_of_neuron_for_nth_input_can_be_got(NeuronPid, Index)).
 
+property_any_neuron_can_be_killed() ->
+   ?FORALL(
+      NeuronPid,
+      neuron_pid(),
+      neuron_can_be_killed(NeuronPid)).
+
+property_any_neurons_bias_can_be_changed() ->
+   ?FORALL(
+      {NeuronPid, Change},
+      {neuron_pid(), real()},
+      bias_of_neuron_can_be_changed(NeuronPid, Change)).
+
+property_any_neurons_weight_for_any_of_its_inputs_can_be_changed() ->
+   ?FORALL(
+      {{NeuronPid, Index}, Change},
+      {neuron_index_pair(), real()},
+      weight_of_neuron_for_nth_input_can_be_changed(NeuronPid, Index, Change)).
+
+property_the_inputs_of_any_neuron_can_be_updated() ->
+   ?FORALL(
+      {NeuronPid, Inputs},
+      neuron_inputs_pair(),
+      inputs_of_neuron_can_be_updated_to(NeuronPid, Inputs)).
+
 % Tests %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 a_neuron_can_be_created_with_input_size(InputSize) ->
@@ -63,6 +91,27 @@ weight_of_neuron_for_nth_input_can_be_got(NeuronPid, Index) ->
    Weight = get_weight(NeuronPid, Index),
    is_float(Weight).
 
+neuron_can_be_killed(NeuronPid) ->
+   Response = kill(NeuronPid),
+   NeuronPid ! {self(), foo},
+   receive
+      _ -> false
+   after 10 ->
+      Response == ok
+   end.
+
+bias_of_neuron_can_be_changed(NeuronPid, Change) ->
+   Response = change_bias(NeuronPid, Change),
+   Response == ok.
+
+weight_of_neuron_for_nth_input_can_be_changed(NeuronPid, Index, Change) ->
+   Response = change_weight(NeuronPid, Index, Change),
+   Response == ok.
+
+inputs_of_neuron_can_be_updated_to(NeuronPid, Inputs) ->
+   Response = update(NeuronPid, Inputs),
+   Response == ok.
+
 % Helper functions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 neuron_pid() ->
@@ -82,4 +131,17 @@ input_size_index_pair() ->
       {InputSize, Index},
       {nat(), nat()},
       Index < InputSize).
+
+neuron_inputs_pair() ->
+   ?LET(
+      InputSize,
+      nat(),
+      {init(InputSize), neuron_inputs(InputSize)}).
+
+neuron_inputs(InputSize) ->
+   RandomFloat = fun(_, _) ->
+      uniform()
+   end,
+   Array = new(InputSize),
+   map(RandomFloat, Array).
 
